81ec7f917ed8aefe774ab2c020fd558e685e93ce
MXrtyht <1922382994@qq.com>
2026-07-17 15:39:46 +0800

    finish: Lab3 Exercise 2

--- src/java/simpledb/optimizer/TableStats.java
+++ src/java/simpledb/optimizer/TableStats.java
@@ -3,11 +3,10 @@ package simpledb.optimizer;
 import simpledb.common.Database;
 import simpledb.common.Type;
 import simpledb.execution.Predicate;
-import simpledb.execution.SeqScan;
 import simpledb.storage.*;
-import simpledb.transaction.Transaction;
+import simpledb.transaction.TransactionId;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -68,6 +67,14 @@ public class TableStats {
      */
     static final int NUM_HIST_BINS = 100;
 
+    private int tableId;
+    private int ioCostPerPage;
+    private DbFile file;
+    private IntHistogram[] intHistogram;
+    private int[] minNum;
+    private int[] maxNum;
+    private int tupleNum;
+
     /**
      * Create a new TableStats object, that keeps track of statistics on each
      * column of a table
@@ -87,6 +94,90 @@ public class TableStats {
         // necessarily have to (for example) do everything
         // in a single scan of the table.
         // some code goes here
+        this.tableId = tableid;
+        this.ioCostPerPage = ioCostPerPage;
+        this.tupleNum = 0;
+
+        this.file = Database.getCatalog().getDatabaseFile(tableid);
+        TupleDesc td = this.file.getTupleDesc();
+
+        int n = td.numFields();
+        this.intHistogram = new IntHistogram[n];
+        this.minNum = new int[n]; Arrays.fill(this.minNum, Integer.MAX_VALUE);
+        this.maxNum = new int[n]; Arrays.fill(this.maxNum, Integer.MIN_VALUE);
+
+        Type[] types = new Type[n];
+        for(int i=0; i<n; i++){
+            types[i] = td.getFieldType(i);
+        }
+
+        TransactionId tid = new TransactionId();
+        DbFileIterator it = file.iterator(tid);
+
+        this.setMinAndMax(n, types, it);
+        this.setIntHistogram(n, types, it);
+        // try{
+        //     it.open();
+        //     while(it.hasNext()){
+        //         Tuple t = it.next();
+        //         this.tupleNum ++;
+        //         for(int i=0; i<n; i++){
+        //             if(types[i] == Type.INT_TYPE){
+        //                 this.intHistogram[i].addValue(((IntField)t.getField(i)).getValue());
+        //             }
+        //         }
+        //         this.tupleNum ++;
+        //     }
+        // }catch(Exception e){
+        //     return;
+        // }
+    }
+
+    private void setMinAndMax(int num, Type[] types, DbFileIterator it){
+        this.minNum = new int[num]; Arrays.fill(this.minNum, Integer.MAX_VALUE);
+        this.maxNum = new int[num]; Arrays.fill(this.maxNum, Integer.MIN_VALUE);
+
+        try{
+            it.rewind();
+            while(it.hasNext()){
+                Tuple t = it.next();
+                for(int i=0; i<num; i++){
+                    if(types[i] == Type.INT_TYPE){
+                        int value = ((IntField)t.getField(i)).getValue();
+                        if(value < this.minNum[i]) this.minNum[i] = value;
+                        if(value > this.maxNum[i]) this.maxNum[i] = value;
+                    }
+                }
+            }
+        }catch(Exception e){
+            return;
+        }
+    }
+
+    private void setIntHistogram(int num, Type[] types, DbFileIterator it){
+        for(int i=0; i<num; i++){
+            this.intHistogram[i] = new IntHistogram(
+                TableStats.NUM_HIST_BINS, 
+                this.minNum[i],
+                this.maxNum[i]
+            );
+        }
+
+        try{
+            it.rewind();
+            while(it.hasNext()){
+                Tuple t = it.next();
+                this.tupleNum ++;
+                for(int i=0; i<num; i++){
+                    if(types[i] == Type.INT_TYPE){
+                        int value = ((IntField)t.getField(i)).getValue();
+                        this.intHistogram[i].addValue(value);
+                    }
+                }
+            }
+        }catch(Exception e){
+            return;
+        }
     }
 
     /**
@@ -103,7 +194,8 @@ public class TableStats {
      */
     public double estimateScanCost() {
         // some code goes here
-        return 0;
+        DbFile file = Database.getCatalog().getDatabaseFile(this.tableId);
+        return (double)(((HeapFile)file).numPages() * this.ioCostPerPage);
     }
 
     /**
@@ -117,7 +209,7 @@ public class TableStats {
      */
     public int estimateTableCardinality(double selectivityFactor) {
         // some code goes here
-        return 0;
+        return (int)(this.tupleNum * selectivityFactor);
     }
 
     /**
@@ -132,7 +224,33 @@ public class TableStats {
      * */
     public double avgSelectivity(int field, Predicate.Op op) {
         // some code goes here
-        return 1.0;
+        double result = 0;
+    
+        // 获取该字段的直方图
+        if (field >= 0 && field < intHistogram.length && intHistogram[field] != null) {
+            IntHistogram hist = intHistogram[field];
+            
+            switch (op) {
+                case EQUALS:
+                    // 平均相等选择率 = 直方图的平均选择率
+                    return hist.avgSelectivity();
+                case NOT_EQUALS:
+                    // 平均不相等选择率 = 1 - 平均相等选择率
+                    return 1.0 - hist.avgSelectivity();
+                case LESS_THAN:
+                case GREATER_THAN:
+                    // 平均来说，一半的元组满足条件
+                    return 0.5;
+                case LESS_THAN_OR_EQ:
+                case GREATER_THAN_OR_EQ:
+                    // 包含等号，略大于0.5
+                    return 0.5 + hist.avgSelectivity();
+                default:
+                    return 0.5;
+            }
+        }
+        
+        return result;
     }
 
     /**
@@ -150,7 +268,20 @@ public class TableStats {
      */
     public double estimateSelectivity(int field, Predicate.Op op, Field constant) {
         // some code goes here
-        return 1.0;
+        if (field < 0 || field >= intHistogram.length || intHistogram[field] == null) {
+            return 1.0;  // 如果没有直方图信息，返回保守估计
+        }
+        
+        // 只处理 INT_TYPE 字段
+        if (constant.getType() != Type.INT_TYPE) {
+            return 1.0;
+        }
+        
+        IntField intField = (IntField) constant;
+        int value = intField.getValue();
+
+        // 使用该字段的直方图来估算选择率
+        return intHistogram[field].estimateSelectivity(op, value);
     }
 
     /**
@@ -158,7 +289,7 @@ public class TableStats {
      * */
     public int totalTuples() {
         // some code goes here
-        return 0;
+        return this.tupleNum;
     }
 
 }
